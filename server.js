4110e52653d018ab04f89a61ca2c3f57982b156e
Pavithra Selvan <pselvan@charlotte.edu>
2025-09-13 20:10:17 -0400

    Added server, Axios, and ChartJS

--- server.js
+++ server.js
@@ -5,25 +5,29 @@ const cors = require('cors');
 const app = express();
 const port = 3000;
 
-app.use(cors());
+app.use('/', express.static('public'));
+// app.use(cors());
 
 const budget = {
     myBudget: [
         {
             title: 'Eat out',
-            budget: 25
+            budget: 90
         },
         {
             title: 'Rent',
-            budget: 275
+            budget: 110
         },
         {
             title: 'Grocery',
-            budget: 110
+            budget: 200
         },
     ]
 };
 
+app.get('/hello', (req, res) => {
+    res.send("Hello World!, This is Pavithra's Personal Budget homepage");
+});
 
 app.get('/budget', (req, res) => {
     res.json(budget);
@@ -32,3 +36,4 @@ app.get('/budget', (req, res) => {
 app.listen(port, () => {
     console.log(`API served at http://localhost:${port}`);
 });
+
