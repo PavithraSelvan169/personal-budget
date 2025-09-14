bafde0ae2749ac66fe2179e57e39dcb7dec559e5
Pavithra Selvan <pselvan@charlotte.edu>
2025-09-13 20:53:34 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -2,36 +2,25 @@
 
 const express = require('express');
 const cors = require('cors');
+
+//import data from JSON file
+const budget_data = require('./budget_data.json')
+
 const app = express();
 const port = 3000;
 
 app.use('/', express.static('public'));
 // app.use(cors());
 
-const budget = {
-    myBudget: [
-        {
-            title: 'Eat out',
-            budget: 90
-        },
-        {
-            title: 'Rent',
-            budget: 110
-        },
-        {
-            title: 'Grocery',
-            budget: 200
-        },
-    ]
-};
+
 
 app.get('/hello', (req, res) => {
     res.send("Hello World!, This is Pavithra's Personal Budget homepage");
 });
 
 app.get('/budget', (req, res) => {
-    res.json(budget);
-});
+    res.json(budget_data);
+    });
 
 app.listen(port, () => {
     console.log(`API served at http://localhost:${port}`);
